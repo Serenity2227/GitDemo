adc941ae5819341750a380563b1e5cc65ec64452
nitin <serenity.serenity2227@gmail.com>
2021-06-06 13:54:33 +0530

    first commit

---
+++ src/main/java/mavenproject/mavenjavaa/AppiumTest.java
@@ -0,0 +1,15 @@
+package mavenproject.mavenjavaa;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void nativeApp() {
+		System.out.println("native app");
+	}
+	@Test
+	public void IOSApp() {
+		System.out.println("IOS app");
+	}
+
+}
---
+++ src/main/java/mavenproject/mavenjavaa/RestAPITest.java
@@ -0,0 +1,16 @@
+package mavenproject.mavenjavaa;
+
+import org.junit.Test;
+
+public class RestAPITest {
+	@Test
+	public void postJira() {
+		System.out.println("postJira");
+	}
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+	}
+
+
+}
